792fce3734122fefeb4c2d143c73359153b44be2
Mark Klein <kleincentral@gmail.com>
2023-11-02 11:37:38 -0500

    Buttons made, push to server is recieving something.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,17 @@ function onReady() {
   console.log("JavaScript is loaded!")
 }
 
+function pushGuesses(event) {
+  event.preventDefault()
+  console.log('Attempt to push to guess')
+  axios({
+    method: 'POST',
+    url: '/highorlow',
+    data: {guess1: 'blank'}
+  }).then(function(response) {
+      console.log("SUCCESS!!!");
+      getList();
+})
+}
+
 onReady()
--- server/server.js
+++ server/server.js
@@ -9,7 +9,19 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+// Can read JSON when passed.
+app.use(express.json());
+
+
 // GET & POST Routes go here
+
+app.get('/highorlow', (req, res) => {
+  console.log('Request was made!')
+})
+
+app.post('/highorlow', (req,res)=> {
+  console.log('Info recieved!')
+})
 
 
 app.listen(PORT, () => {
